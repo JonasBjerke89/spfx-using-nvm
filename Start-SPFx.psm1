efde289f727a17f4ff819cdf62f6a761fb3dfc4b
JonasBjerke89 <jonas.v.bjerke@gmail.com>
2019-01-09 14:31:24 +0100

    Add files via upload

---
+++ Start-SPFx.psm1
@@ -0,0 +1,78 @@
+Function Start-SPFx()
+{
+    Param(  
+    [Parameter(Mandatory=$True)]
+    [String]$version
+    )
+
+    $nodeJSVersion = ""
+    $generator = ""
+
+    switch ($version)
+    {        
+        "1.7.1" 
+        {
+            $nodeJSVersion = "8.12.0"
+            $generator = "@microsoft/generator-sharepoint@1.7.1"
+        }
+        "1.7.0" 
+        {
+            $nodeJSVersion = "8.12.0"
+            $generator = "@microsoft/generator-sharepoint@1.7.0"           
+        }   
+        "1.6.0"
+        {
+            $nodeJSVersion = "8.11.3"
+            $generator = "@microsoft/generator-sharepoint@1.6.0"
+
+        }
+        "1.5.1"
+        {
+            $nodeJSVersion = "8.11.3"
+            $generator = "@microsoft/generator-sharepoint@1.5.1"
+
+        }
+        "1.5.0"
+        {
+            $nodeJSVersion = "8.11.3"
+            $generator = "@microsoft/generator-sharepoint@1.5.0"
+
+        }
+        "1.4.1"
+        {
+            $nodeJSVersion = "8.9.4"
+            $generator = "@microsoft/generator-sharepoint@1.4.1"
+
+        }    
+        "1.4.0"
+        {
+            $nodeJSVersion = "6.12.1"
+            $generator = "@microsoft/generator-sharepoint@1.4.0"
+
+        }
+        "1.3.4"
+        {
+            $nodeJSVersion = "6.11.5"
+            $generator = "@microsoft/generator-sharepoint@1.3.4"
+
+        }   
+    }
+    
+    nvm install $nodeJSVersion
+    nvm use $nodeJSVersion
+
+    if(!(Test-Path -Path ".\node_modules" ))
+    {
+        Write-Host "node_modules not found, installing SPFx prerequisites!" -ForegroundColor Yellow
+        Start-Sleep -s 2
+
+        npm install -g yo gulp $generator   
+
+	if((Test-Path -Path ".\package.json" ))
+    	{
+            npm install # PROJECT SPECIFIC PACKAGES IF IT IS AN EXISTING PROJECT
+    	}     
+    }    
+
+    Write-Host "Go Develop using SPFx!" -ForegroundColor Green
+}
